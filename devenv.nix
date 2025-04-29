66461b595ac633067c8c79d15003478eb53a0270
John Children <john.children@quantinuum.com>
2025-04-29 13:18:49 +0100

    docs: Add README sample code

--- devenv.nix
+++ devenv.nix
@@ -1,22 +1,23 @@
 { pkgs, lib, config, inputs, ... }:
 let
-  qulacs = pkgs.callPackage ./nix/qulacs.nix {};
-in {
+  qulacs = pkgs.callPackage ./nix/qulacs.nix { };
+in
+{
   env = {
     QULACS_PATH = qulacs;
     LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
   };
-  
-   packages = [
-     pkgs.boost
-     pkgs.eigen
-     pkgs.mpi
-     qulacs
-     
-     pkgs.release-plz
-     pkgs.cargo-semver-checks
-   ];
- 
+
+  packages = [
+    pkgs.boost
+    pkgs.eigen
+    pkgs.mpi
+    qulacs
+
+    pkgs.release-plz
+    pkgs.cargo-semver-checks
+  ];
+
   languages.rust.enable = true;
   languages.nix.enable = true;
 }
