32cddbe6649c359dc7795140349b97785b9e760f
John Children <32305209+johnchildren@users.noreply.github.com>
2025-04-29 13:30:37 +0100

    docs: Add README sample code (#34)
    * fix: Correct link directive
    
    * fix: Use a raw loop
    
    Rob Pike would be proud
    
    * fix: Correct flag usage
    
    * docs: Add README sample code

--- devenv.nix
+++ devenv.nix
@@ -1,18 +1,19 @@
 { pkgs, lib, config, inputs, ... }:
 let
-  qulacs = pkgs.callPackage ./nix/qulacs.nix {};
-in {
+  qulacs = pkgs.callPackage ./nix/qulacs.nix { };
+in
+{
   env = {
     QULACS_PATH = qulacs;
     LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
   };
-  
+
   packages = [
     pkgs.boost
     pkgs.eigen
     pkgs.mpi
     qulacs
-    
+
     pkgs.release-plz
     pkgs.cargo-semver-checks
   ];
